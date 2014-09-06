daa2acf719bf8e5e1f14880eff86f03bdbbff28a
deanoj <deanoj@deanoj.co.uk>
2014-09-06 10:17:11 +0100

    initial import

---
+++ src/Calculator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deanoj
+ * Date: 25/08/2014
+ * Time: 14:29
+ */
+
+namespace Deanoj\TestProject1;
+
+class Calculator {
+
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+} 
---
+++ test/CalculatorTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deanoj
+ * Date: 25/08/2014
+ * Time: 14:38
+ */
+
+namespace Deanoj\TestProject1;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testFalse()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testAdd()
+    {
+        $calc = new Calculator();
+
+        $this->assertEquals(13, $calc->add(6,7));
+        $this->assertEquals(7, $calc->add(4,3));
+    }
+
+
+} 
